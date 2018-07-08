6e39571993711dadc645efbeae70c30fb75361f5
Jefreesujit <e.jefree110@gmail.com>
2018-07-08 18:50:05 +0530

    added basic test cases

--- src/__tests__/sample-test.js
+++ src/__tests__/sample-test.js
@@ -1,8 +1,44 @@
 /* global describe it expect */
+import { mount, shallow } from 'enzyme';
+import expect from 'expect';
+import createStore from '../src';
 
-describe('Sample Test', () => {
-  const someVal = 1;
-  it('should pass', () => {
-    expect(someVal === 1).toBe(true);
+// describe('Sample Test', () => {
+//   const someVal = 1;
+//   it('should pass', () => {
+//     expect(someVal === 1).toBe(true);
+// describe("Sample Test", () => {
+//   it("should pass", () => {
+//     expect(1 === 1).toBe(true);
+// describe("Sample Test", () => {
+//   it("should pass", () => {
+//     expect(1 === 1).toBe(true);
+//   });
+// });
+
+let defaultState = {
+      example: {
+        value: 0
+      }
+    },
+    { Provider, connect } = createStore(defaultState);
+
+
+describe("Checking Store creation", () => {
+  it("Provider and connect are properly defined", () => {
+    expect(Provider).toBeDefined();
+    expect(connect).toBeDefined();
+  });
+});
+
+describe("Checking Rendering", () => {
+  let Component = shallow(<Provider><div/></Provider>);
+
+  it('Provider is rendered', () => {
+    expect((Component).length).toEqual(1);
+  });
+
+  it('Elements inside Provider is rendered', () => {
+    expect((Component).find('div').length).toEqual(1);
   });
 });
