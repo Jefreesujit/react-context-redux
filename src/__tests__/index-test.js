3fc3e5c2244aef20349b48e5976c73e3d1cb8c2c
Jefreesujit <e.jefree110@gmail.com>
2018-07-08 21:27:58 +0530

    added few test cases

---
+++ src/__tests__/index-test.js
@@ -0,0 +1,60 @@
+/* global describe it expect */
+import React from 'react';
+import { shallow } from 'enzyme';
+import createStore from '../CreateStore';
+
+const defaultState = { count: 0 };
+
+const { Provider, connect } = createStore(defaultState);
+
+const select = state => ({ count: state.count });
+
+const Counter = ({ count }) => (
+  <button
+    type="button"
+    onClick={() => dispatch => dispatch({ key: 'count', value: count + 1 })}
+  >
+    {count}
+  </button>
+);
+
+Counter.displayName = 'Counter';
+
+describe('Checking Store creation', () => {
+  it('Provider and connect are properly defined', () => {
+    expect(Provider).toBeDefined();
+    expect(connect).toBeDefined();
+  });
+});
+
+describe('Checking Rendering', () => {
+  let Component;
+  let ConnectedComp;
+
+  describe('Actual Component Rendering', () => {
+    ConnectedComp = connect(select)(Counter);
+    Component = shallow(
+      <Provider>
+        <ConnectedComp test="test" />
+      </Provider>
+    );
+    const CounterWrapper = Component.find('Connect(Counter)');
+
+    it('Provider is rendered', () => {
+      expect(Component.length).toEqual(1);
+    });
+
+    it('Checking if Provider has default state set', () => {
+      expect(Component.prop('value')).toEqual(defaultState);
+    });
+
+    it('Counter wrapper component is rendered', () => {
+      expect(CounterWrapper.length).toEqual(1);
+    });
+
+    it('Counter component is rendered', () => {
+      console.log(CounterWrapper.debug());
+      // expect((CounterWrapper).find('Counter').length).toEqual(1);
+    });
+  });
+});
