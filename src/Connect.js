078b9e6f3b99b117a5e78562af6eed64c54e83f2
Jefreesujit <e.jefree110@gmail.com>
2018-07-14 21:16:52 +0530

    added basic jsdoc info

--- src/Connect.js
+++ src/Connect.js
@@ -4,6 +4,13 @@ const connect = (
   Consumer,
   dispatcher
 ) => mapStateToProps => WrappedComponent => {
+  /**
+   * Connects the passed component with the global store.
+   * @param {function} select - To map global state to props, should return an JSON object
+   * @param {Node} component - A React element, to which the props should be passed
+   * @returns {Node}  - A wrapper React element
+   */
+
   const dispatchProp = actionCallback => {
     if (typeof actionCallback === 'function') {
       actionCallback(dispatcher);
--- src/CreateStore.js
+++ src/CreateStore.js
@@ -2,7 +2,17 @@ import { createContext } from 'react';
 import createProvider from './Provider';
 import createConnect from './Connect';
 
+/** CreateStore function
+ * @exports CreateStore
+ */
+
 const createStore = initialState => {
+  /**
+   * Creates an global store.
+   * @param {Object} defaultState - The default state of the application need to create the sote
+   * @returns {Node, function}  - Returns provider component and connect function
+   */
+
   let _updateState;
 
   const { Provider, Consumer } = createContext();
